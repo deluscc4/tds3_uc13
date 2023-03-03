623d94d75431c305a1cfb72eac7dc458f644fcc7
deluscc4 <delusccafs@icloud.com>
2023-03-02 21:50:55 -0300

    aula 2

---
+++ aula02/exe02.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Hello, world!";
+?>
+
+<h1>CURSO TDS SENAC</h1>
+<?php
+    echo "Sou aluno TDS";
+?>
---
+++ aula02/exercicio.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = 2;
+$num2 = 6;
+$soma = $num1 + $num2;
+
+if ($soma > 20) {
+    echo "$soma" + 8;
+} else if ($soma <= 20) {
+    echo "$soma" - 5;
+}
+?>
